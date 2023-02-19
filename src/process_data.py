c6fe93a737bb06c0c9a5b61146df52d0895f2427
kdunorat <kdu.norat@gmail.com>
2023-02-19 12:18:20 -0300

    Fixing error handling

--- src/process_data.py
+++ src/process_data.py
@@ -6,34 +6,33 @@ import os
 def create_data(filename):
     """Cria um dataframe pandas a partir de um arquivo sam"""
     check = input_check(filename)
-    if check:
-        soft_sam(filename)
-        path = 'input/soft_cliped.sam'
-
-        with open(path, 'r') as f:
-            lines = f.readlines()
-        data = []
-        for line in lines:
-            fields = line.strip().split('\t')
-            row = {
-                'qname': fields[0],
-                'flag': int(fields[1]),
-                'rname': fields[2],
-                'pos': int(fields[3]),
-                'fpos': int(fields[3]) + len(fields[9]),
-                'cigar': fields[5],
-                'seq': fields[9],
-                'qual': fields[10],
-            }
-            data.append(row)
-
-        df = pd.DataFrame(data)
-        os.remove("input/soft_cliped.sam")
-        return df
-    else:
+    if not check:
         raise WrongOrMissingInput(f"The file {filename} either could not "
                                   f"been found in input folder"
                                   f" or it's in the wrong format")
+    soft_sam(filename)
+    path = 'input/soft_cliped.sam'
+
+    with open(path, 'r') as f:
+        lines = f.readlines()
+    data = []
+    for line in lines:
+        fields = line.strip().split('\t')
+        row = {
+            'qname': fields[0],
+            'flag': int(fields[1]),
+            'rname': fields[2],
+            'pos': int(fields[3]),
+            'fpos': int(fields[3]) + len(fields[9]),
+            'cigar': fields[5],
+            'seq': fields[9],
+            'qual': fields[10],
+        }
+        data.append(row)
+
+    df = pd.DataFrame(data)
+    os.remove("input/soft_cliped.sam")
+    return df
 
 
 def input_check(filename):
